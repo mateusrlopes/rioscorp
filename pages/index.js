402de2a5c6338836eee9279eadbea8baa5b22217
Mateus Rios <mateusrioslopes@hotmail.com>
2024-12-30 18:41:09 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Esse pode ser o nosso futuro forte!</h1>
+    return <h1>O futuro é imprevisível mas se torna brilhante conforme o tempo passa!</h1>
 } 
 
 export default Home;
